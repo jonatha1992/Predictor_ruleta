f1f1172e25e36569c6e6db04a79a6d9db0414f43
jonathan correa <jonathancorrea291292@hotmail.com>
2023-11-27 23:59:45 -0300

    confirmar cambios

--- programa.py
+++ programa.py
@@ -257,42 +257,43 @@ class Predictor:
 
 # Función principal que ejecuta el programa.
 def main():
-    excel_datos = input("\nIngresa el excel de datos: ")
-    
-    if not os.path.exists(excel_datos):
-        print("El archivo Excel no existe. No se puede instanciar el Predictor.")
-        return
-    
-    predictor = Predictor(excel_datos)
-    numerosingresado= []
     while True:
-        opcion = input(
-            "\nIngresa un nuevo número o 'salir' para terminar el programa: "
-        )
-        if opcion.lower() == "salir":
-            print(numerosingresado)
-            predictor.guardar_excel()
-            os.system("start excel Reportes.xlsx")
-            break
+        excel_datos = input("\nIngresa el excel de datos: ")
+        
+        if not os.path.exists(excel_datos):
+            print("El archivo Excel no existe. No se puede instanciar el Predictor.")
+            return
+        predictor = Predictor(excel_datos)
+        numerosingresado= []
+        
+        while True:
+            opcion = input(
+                "\nIngresa un nuevo número o 'salir' para terminar el programa: "
+            )
+            if opcion.lower() == "salir":
+                print(numerosingresado)
+                predictor.guardar_excel()
+                os.system("start excel Reportes.xlsx")
+                break
 
-        if opcion.lower() == "-":
-            predictor.borrar()
-            continue
-        try:
-            numero = int(opcion)
-            numerosingresado.append(numero)
-            
-            if numero < 0 or numero > 36:
-                print("El número debe estar entre 0 y 36. Inténtalo nuevamente.")
+            if opcion.lower() == "-":
+                predictor.borrar()
                 continue
-            
-            predictor.verificar_numero(numero)
-            predictor.predecir()
-            predictor.actualizar_dataframe(numero)
-            predictor.mostrar_resultados()
+            try:
+                numero = int(opcion)
+                numerosingresado.append(numero)
+                
+                if numero < 0 or numero > 36:
+                    print("El número debe estar entre 0 y 36. Inténtalo nuevamente.")
+                    continue
+                
+                predictor.verificar_numero(numero)
+                predictor.predecir()
+                predictor.actualizar_dataframe(numero)
+                predictor.mostrar_resultados()
 
-        except ValueError:
-            print("Valor ingresado no válido. Inténtalo nuevamente.")
+            except ValueError:
+                print("Valor ingresado no válido. Inténtalo nuevamente.")
 
 # Si el script se ejecuta como programa principal, llama a la función main().
 if __name__ == "__main__":
