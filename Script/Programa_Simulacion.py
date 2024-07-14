e5ea15e5daa2e6eed2baced2e080c51675ec3158
jonathan correa <jonathancorrea291292@hotmail.com>
2024-07-14 19:02:18 -0300

    cambios de arcihvos eliminacion .env

--- Script/Programa_Simulacion.py
+++ Script/Programa_Simulacion.py
@@ -3,7 +3,7 @@ import itertools
 from Entity.Contador import Contador
 from Entity.Parametro import Parametro_Juego
 from Entity.Predictor import Predictor
-from Numeros_Simulacion import Simulador
+from Script.Numeros_Simulacion import Simulador
 
 import pandas as pd
 
