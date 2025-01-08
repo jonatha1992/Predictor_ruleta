9f3d1ed7516c9f88d329e24c946f2e365ddbf705
jonathan-escritorio <joni92@gmail.com>
2025-01-08 03:47:03 -0300

    Eliminar la columna 'vecino2' y ajustar el cálculo de características en el modelo de predicción

--- Entity/Modelo.py
+++ Entity/Modelo.py
@@ -96,16 +96,13 @@ class Modelo:
 
         # Añadir características adicionales
         self.df['vecino1'] = self.df['Salidos'].apply(lambda numero: vecino1lugar.get(numero, []))
-        self.df['vecino2'] = self.df['Salidos'].apply(lambda numero: vecino2lugar.get(numero, []))
         self.df['sector'] = self.df['Salidos'].apply(determinar_sector)
-        # Codificar la columna 'sector'
         self.df['sector_encoded'] = le.fit_transform(self.df['sector'])
 
         # Calcular frecuencias y preparar datos
         self.df = calcular_frecuencia(self.df, rango=10)
         self.numeros = self.df["Salidos"].values.tolist()
         self.vecinos1 = self.df["vecino1"].values.tolist()
-        self.vecinos2 = self.df["vecino2"].values.tolist()
         self.frecuencias = self.df["Frecuencia"].values.tolist()
 
         # Añadir estadísticas avanzadas
@@ -182,7 +179,6 @@ class Modelo:
                     self.numeros[idx],       # Número actual
                     self.frecuencias[idx],   # Frecuencia en rango
                     int(self.numeros[idx] in self.vecinos1[idx]),
-                    int(self.numeros[idx] in self.vecinos2[idx]),
                     self.df['sector_encoded'].iloc[idx],  # Sector codificado
                     self.media_ponderada[idx],     # Nueva característica
                     self.velocidad_cambio[idx],    # Nueva característica
