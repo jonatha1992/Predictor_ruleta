9a8bbe8b93799cc8955d591749a1cd2048038e39
jonathan correa <jonathancorrea291292@hotmail.com>
2025-01-26 15:57:09 -0300

    Actualizar hiperparámetros en la clase HiperParametros, ajustando valores de capas, tasa de aprendizaje, y número de épocas.

--- Entity/Parametro.py
+++ Entity/Parametro.py
@@ -2,15 +2,15 @@ class HiperParametros:
     # Inicializa el objeto de la clase con un nombre de archivo y crea el modelo.
     def __init__(self, **kwargs):
         # hiperparametros
-        self.numerosAnteriores = kwargs.get("numerosAnteriores", 8)
-        self.capa1 = kwargs.get("capa1", 128)  # Mejor valor encontrado para rnn_units
-        self.capa2 = kwargs.get("capa2", 192)
-        self.capa3 = kwargs.get("capa3", 96)
-        self.l2_lambda = kwargs.get("l2_lambda", 0.00988979690132718)  # Mejor valor encontrado
-        self.dropout_rate = kwargs.get("dropout_rate", 0.24029634193965138)  # Mejor valor encontrado
-        self.learning_rate = kwargs.get("learning_rate", 0.00988979690132718)  # Mejor valor encontrado
-        self.epochs = kwargs.get("epochs", 100)  # Mejor valor encontrado
-        self.batchSize = kwargs.get("batchSize", 384)
+        self.numerosAnteriores = kwargs.get("numerosAnteriores", 10)
+        self.capa1 = kwargs.get("capa1", 384)  # Mejor valor encontrado para rnn_units
+        self.capa2 = kwargs.get("capa2", 256)
+        self.capa3 = kwargs.get("capa3", 128)
+        self.l2_lambda = kwargs.get("l2_lambda", 0.008640737182527825)  # Mejor valor encontrado
+        self.dropout_rate = kwargs.get("dropout_rate", 0.3875188255460017)  # Mejor valor encontrado
+        self.learning_rate = kwargs.get("learning_rate", 0.00016993366812604327)  # Mejor valor encontrado
+        self.epochs = kwargs.get("epochs", 200)  # Mejor valor encontrado
+        self.batchSize = kwargs.get("batchSize", 256)
 
 
 class Parametro_Juego:
